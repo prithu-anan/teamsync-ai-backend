66bd4ac543aaaaffef3f5f81059e662b30b2324c
Prithu Anan <prithua00@gmail.com>
2025-06-25 05:50:35 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import health, task , channel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # <-- Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # <-- Allow all methods: GET, POST, etc.
+    allow_headers=["*"],  # <-- Allow all headers
+)
 app.include_router(health.router)
 app.include_router(task.router)
 app.include_router(channel.router)
